8463002ed157c85dc6997f9aa72fd8888b232a00
n1tjrgns <n1tjrgns@naver.com>
2019-03-08 17:35:56 +0900

    @Value 수정

--- src/main/java/com/example/batch/seokhun/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/batch/seokhun/job/SimpleJobConfiguration.java
@@ -1,21 +1,22 @@
 package com.example.batch.seokhun.job;
 
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.logging.Logger;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Slf4j //log 사용을 위한 lombok 어노테이션
 @RequiredArgsConstructor //생성자 DI를 위한 lombok 어노테이션
@@ -24,8 +25,12 @@ import java.util.logging.Logger;
 
 public class SimpleJobConfiguration {
 
+    @Autowired
+    JdbcTemplate jdbc;
+
     private final JobBuilderFactory jobBuilderFactory; //생성자 DI 받음
     private final StepBuilderFactory stepBuilderFactory; //생성자 DI 받음
+
 
     @Bean
     public Job simpleJob(){
@@ -34,25 +39,28 @@ public class SimpleJobConfiguration {
         //Builder를 통해 job의 이름을 지정
         //job = 하나의 배치 작업 단위
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
     }
 
+
     @Bean
-    public Step simpleStep1() {
-        System.out.println("실행이 안되는데?");
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         //simpleStep1이라는 Batch Step 생성
         return stepBuilderFactory.get("simpleStep1")
                 /*/.tasklet : Step 안에서 단일로 수행될 커스텀한 기능을 선언할 때 사용
                 step 안에서 수행될 기능을 명시
                 여기서는 Batch 수행시 step1 이라는 log.info가 수행됨됨
                 */
+
                .tasklet(new Tasklet() {
                    //tasklet 은 execute 메소드를 구현, 비즈니스 로직을 실행하는 코드를 작성, 리턴값으로 RepeatStatus.Finished를 사용
                    @Override
                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                        log.info(">>>>This is Step1");
-                       System.out.println("this is step");
+                       log.info(">>>>requestDate = {}", requestDate);
+                       System.out.println(jdbc);
                        return RepeatStatus.FINISHED;
                    }
                })
