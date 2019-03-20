5f8647c403292712776fb47676e633c310bf3374
n1tjrgns <n1tjrgns@naver.com>
2019-03-20 15:15:30 +0900

    ItemProcessor - 필터 예제

---
+++ src/main/java/com/example/batch/seokhun/processor/ProcessorNullJobConfiguration.java
@@ -0,0 +1,127 @@
+package com.example.batch.seokhun.processor;
+
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class ProcessorNullJobConfiguration {
+
+    public static final String JOB_NAME = "processorNullBatch";
+    public static final String BEAN_PREFIX = JOB_NAME + "_";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+
+    @Value("${chunkSize:1000}")
+    private int chunkSize;
+
+    @Bean(JOB_NAME)
+    public Job job() throws  Exception{
+        return jobBuilderFactory.get(JOB_NAME)
+                .preventRestart()
+                .start(step())
+                .build();
+    }
+
+    @Bean(BEAN_PREFIX+"step")
+    @JobScope
+    public Step step() throws Exception{
+        return stepBuilderFactory.get(BEAN_PREFIX+"step")
+                .<Teacher, String>chunk(chunkSize)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Teacher> reader() throws Exception{
+
+        Map<String, Object> parameterValues = new HashMap<>();
+        parameterValues.put("id", 2);
+
+
+        return new JdbcPagingItemReaderBuilder<Teacher>()
+                .fetchSize(chunkSize)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Teacher.class))
+                .queryProvider(createQueryProvider())
+                .name("jdbcPagingItemReader")
+                .build();
+
+    }
+
+    @Bean
+    // @ConditionalOnMissingBean //프로젝트 상에서 동명의 스프링 빈이 정의되었을 시 쓰지않고
+    //정의된 스프링 빈을 사용하고, 없을 시 자동 등록한 빈을 쓰게 유도함.
+    public PagingQueryProvider createQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource);
+        queryProvider.setSelectClause("id, name");
+        queryProvider.setFromClause("from teacher");
+        //queryProvider.setWhereClause("where id >= :id");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        return queryProvider.getObject();
+    }
+
+    @Bean
+    public ItemProcessor<Teacher, String > processor(){
+       return new ItemProcessor<Teacher, String>() {
+           @Override
+           public String process(Teacher teacher) throws Exception {
+               boolean isIgnoreTarget = teacher.getId() % 2 == 0L;
+               if (isIgnoreTarget) {
+                   log.info(">>>>>>>> Teacher name = {}, isIgnoreTarget={}", teacher.getName(), isIgnoreTarget);
+                   return null;
+               }
+               return String.valueOf(teacher);
+           }
+       };
+    }
+
+    private ItemWriter<String> writer(){
+        return new ItemWriter<String>() {
+            @Override
+            public void write(List<? extends String> items) throws Exception {
+                for (String item : items) {
+                    log.info("Teacher Name={}", item);
+                }
+            }
+        };
+    }
+}
